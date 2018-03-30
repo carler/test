3d806cd32e9a012c6df35fd4bbab065f694e289b
root <root@localhost.localdomain>
2018-03-30 11:14:03 +0800

    upload code

---
+++ src/Ford/Escape/Escape2018.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape;
+
+class Escape2018
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2018!<br />";
+    }
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Ford\Escape as Escape;
+
+echo Escape\Escape2018::info();
